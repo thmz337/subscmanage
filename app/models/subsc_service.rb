6261d90bf1cfc9551789d4210f206de5789b6328
thmz337 <th994209@gmail.com>
2025-11-24 22:37:20 +0900

    サービスにバリデーションを追加

--- app/models/subsc_service.rb
+++ app/models/subsc_service.rb
@@ -1,2 +1,14 @@
 class SubscService < ApplicationRecord
+  validates :name, presence: true
+  validates :next_payment, presence: true, comparison: { greater_than_or_equal_to: Date.today }
+  validates :payment_interval, presence: true, comparison: { greater_than: 0 }
+  validates :payment_unit, presence: true,
+  inclusion: { in: ->(service) {
+    service.available_payment_units
+  } }
+  validates :price, presence: true, comparison: { greater_than_or_equal_to: 0 }
+
+  def available_payment_units
+    %w[month year]
+  end
 end
