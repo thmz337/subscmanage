752e50d5ca8b6b34a818fe32bf88dffa7ae9a6ba
thmz337 <th994209@gmail.com>
2025-11-28 11:49:49 +0900

    devise installerの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_22_132147) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_28_023354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,5 +22,17 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_22_132147) do
     t.string "payment_unit"
     t.integer "price"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
