6dba267e378cef35b164d6487f22ca0d19af14c1
thmz337 <th994209@gmail.com>
2025-12-03 22:19:44 +0900

    deviseの導入

--- app/controllers/subscription_services_controller.rb
+++ app/controllers/subscription_services_controller.rb
@@ -1,4 +1,5 @@
 class SubscriptionServicesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_subscription_service, only: %i[ show edit update destroy ]
 
   def index
