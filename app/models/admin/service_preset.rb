466e03ff443447684cc2f68043915a4a9a6e913d
thmz337 <th994209@gmail.com>
2025-12-21 19:02:57 +0900

    支払単位をenumで管理する

--- app/models/admin/service_preset.rb
+++ app/models/admin/service_preset.rb
@@ -1,6 +1,7 @@
 class Admin::ServicePreset < ApplicationRecord
+  enum :payment_unit, [ :month, :year ], validate: true
+
   validates :name, presence: true
   validates :payment_interval, comparison: { greater_than: 0 }
-  validates :payment_unit, presence: true, numericality: { in: 0..1 }
   validates :price, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 end
--- app/models/subscription_service.rb
+++ app/models/subscription_service.rb
@@ -1,11 +1,10 @@
 class SubscriptionService < ApplicationRecord
   belongs_to :user
 
-  enum :payment_unit, [ :month, :year ]
+  enum :payment_unit, [ :month, :year ], validate: true
 
   validates :name, presence: true
   validates :next_payment, comparison: { greater_than_or_equal_to: Date.today }
   validates :payment_interval, comparison: { greater_than: 0 }
-  validates :payment_unit, presence: true, numericality: { in: 0..1 }
   validates :price, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 end
