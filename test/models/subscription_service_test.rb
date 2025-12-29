59937117b46677358c5a2a1d86ec397471b8cbfc
thmz337 <th994209@gmail.com>
2025-12-29 22:05:59 +0900

    subscription_serviceモデルに関するテストを作成

--- test/models/subscription_service_test.rb
+++ test/models/subscription_service_test.rb
@@ -1,4 +1,8 @@
 require "test_helper"
 
 class SubscriptionServiceTest < ActiveSupport::TestCase
+  test "should filter by next payment date" do
+    subscription_service = SubscriptionService.payment_for_that_day(Date.new(2026, 1, 1))
+    assert_equal 1, subscription_service.count
+  end
 end
