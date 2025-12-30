6983a66923876d7b501634dc4a34a4b16db4721a
thmz337 <th994209@gmail.com>
2025-12-30 22:57:55 +0900

    プリセットの自動入力について、エンター押下時にも対応

--- app/javascript/controllers/preset_controller.js
+++ app/javascript/controllers/preset_controller.js
@@ -1,9 +1,39 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-	fill({ params: { service: { price, payment_interval, payment_unit } } }) {
-		document.getElementById("subscription_service_price").value = price
-		document.getElementById("subscription_service_payment_interval").value = payment_interval
-		document.getElementById("subscription_service_payment_unit").value = payment_unit
-	}
+  initialize() {
+    this.selectedItem = null;
+  }
+
+  fill(event) {
+    const subscription_service_price = document.getElementById(
+      "subscription_service_price",
+    );
+    const service_payment_interval = document.getElementById(
+      "subscription_service_payment_interval",
+    );
+    const service_payment_unit = document.getElementById(
+      "subscription_service_payment_unit",
+    );
+
+    if (event.type === "click") {
+      subscription_service_price.value = event.params.service.price;
+      service_payment_interval.value = event.params.service.payment_interval;
+      service_payment_unit.value = event.params.service.payment_unit;
+    } else if (event.type === "keydown" && event.key === "Enter") {
+      if (this.selectedItem) {
+        const params = JSON.parse(
+          this.selectedItem.getAttribute("data-preset-service-param"),
+        );
+        subscription_service_price.value = params.price;
+        service_payment_interval.value = params.payment_interval;
+        service_payment_unit.value = params.payment_unit;
+      }
+    }
+  }
+
+  select() {
+    // stimulus-autocomplete
+    this.selectedItem = document.querySelector("[aria-selected='true']");
+  }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,8 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "tailwindcss-stimulus-components" # @6.1.3
 pin "stimulus-autocomplete" # @3.1.0
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "tailwindcss-stimulus-components" # @6.1.3
