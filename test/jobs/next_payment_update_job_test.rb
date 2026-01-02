5634df8f14b5590905104b98da1d5f9b69bd96c7
thmz337 <th994209@gmail.com>
2026-01-02 10:55:03 +0900

    次回支払日の更新に係るジョブのテストを作成

--- test/jobs/next_payment_update_job_test.rb
+++ test/jobs/next_payment_update_job_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class NextPaymentUpdateJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "services with payment dates one day before or earlier than the job execution date must be update" do
+    travel_to Time.zone.local(2026, 1, 1) do
+      perform_enqueued_jobs do
+        NextPaymentUpdateJob.perform_later
+      end
+
+      assert_equal subscription_services(:service3).next_payment.to_s, "2026-01-31"
+    end
+
+    travel_to Time.zone.local(2026, 1, 2) do
+      perform_enqueued_jobs do
+        NextPaymentUpdateJob.perform_later
+      end
+
+      assert_equal subscription_services(:service4).next_payment.to_s, "2027-01-01"
+    end
+  end
+
+  test "services with payment dates after the job execution date should not be update" do
+    travel_to Time.zone.local(2026, 1, 1) do
+      perform_enqueued_jobs do
+        NextPaymentUpdateJob.perform_later
+      end
+
+      assert_equal subscription_services(:service1).next_payment.to_s, "2026-01-01"
+      assert_equal subscription_services(:service2).next_payment.to_s, "2026-02-01"
+    end
+  end
 end
