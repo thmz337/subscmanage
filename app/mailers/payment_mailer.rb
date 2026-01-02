7834f532b9f9ef34aaeb8e6a4dd9d8be63de8458
thmz337 <th994209@gmail.com>
2026-01-02 09:43:14 +0900

    次回支払の通知を行うジョブを修正

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -1,6 +1,8 @@
 class PaymentMailer < ApplicationMailer
   def this_month_payment
     @user = params[:user]
+    @this_month_payment_services = @user.this_month_payment_services
+    @this_month_payment = @this_month_payment_services.map { |service| service.price }.sum
     mail(to: @user.email, subject: "今月の支払いについて")
   end
 end
