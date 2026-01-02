5fe5cdbd1604bfeb3fe8ec2854d516f8e4c22c5a
thmz337 <th994209@gmail.com>
2026-01-02 16:05:37 +0900

    支払通知メールジョブのテストを作成

--- test/jobs/this_month_payment_reminder_job_test.rb
+++ test/jobs/this_month_payment_reminder_job_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ThisMonthPaymentReminderJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionMailer::TestHelper
+
+  test "should send an email to all users regarding this month's payment" do
+    travel_to Time.zone.local(2026, 1, 1) do
+      assert_emails 3 do
+        perform_enqueued_jobs do
+          ThisMonthPaymentReminderJob.perform_later
+        end
+      end
+    end
+  end
 end
