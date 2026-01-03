ef5bbc8058b2487b3cce3ff466b5e1cb54bca72c
thmz337 <th994209@gmail.com>
2026-01-03 09:31:56 +0900

    prettierによる整形を反映

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,7 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "controllers/application";
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+eagerLoadControllersFrom("controllers", application);
 
-import { Tabs } from "tailwindcss-stimulus-components"
-application.register('tabs', Tabs)
+import { Tabs } from "tailwindcss-stimulus-components";
+application.register("tabs", Tabs);
