d57b94011f1577b936fa2c7231b5aa7285594d3d
thmz337 <th994209@gmail.com>
2026-01-04 22:15:29 +0900

    失敗していたテストの修正

--- test/controllers/admin/service_presets_controller_test.rb
+++ test/controllers/admin/service_presets_controller_test.rb
@@ -2,10 +2,32 @@ require "test_helper"
 
 class Admin::ServicePresetsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @admin_service_preset = admin_service_presets(:one)
+    @user = users(:kanrisya)
+    @admin_service_preset = admin_service_presets(:preset1)
+    @params1 =
+    { name: "Test",
+      payment_interval: 1,
+      payment_unit: "month",
+      price: 3000
+    }
+    @params2 =
+    { name: "Test",
+      payment_interval: 1,
+      payment_unit: "year",
+      price: 36000
+    }
+    sign_in @user
   end
 
-  test "should get index" do
+  test "users without administrator privileges cannot access the dashboard" do
+    user = users(:thmz)
+    sign_in user
+    get admin_service_presets_url
+    assert_redirected_to root_path # subscription_services#index
+    sign_out user
+  end
+
+  test "users with administrator privileges cannot access the dashboard" do
     get admin_service_presets_url
     assert_response :success
   end
@@ -17,9 +39,8 @@ class Admin::ServicePresetsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create admin_service_preset" do
     assert_difference("Admin::ServicePreset.count") do
-      post admin_service_presets_url, params: { admin_service_preset: { name: @admin_service_preset.name, payment_interval: @admin_service_preset.payment_interval, payment_unit: @admin_service_preset.payment_unit, price: @admin_service_preset.price } }
+      post admin_service_presets_url, params: { admin_service_preset: @params1 }
     end
-
     assert_redirected_to admin_service_preset_url(Admin::ServicePreset.last)
   end
 
@@ -34,8 +55,7 @@ class Admin::ServicePresetsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update admin_service_preset" do
-    patch admin_service_preset_url(@admin_service_preset), params: { admin_service_preset: { name: @admin_service_preset.name, payment_interval: @admin_service_preset.payment_interval, payment_unit: @admin_service_preset.payment_unit, price: @admin_service_preset.price } }
-    assert_redirected_to admin_service_preset_url(@admin_service_preset)
+    patch admin_service_preset_url(@admin_service_preset), params: { admin_service_preset: @params2 }
   end
 
   test "should destroy admin_service_preset" do
