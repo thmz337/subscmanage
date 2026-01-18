ebcf214c6de70235921585f0485282023235403b
thmz337 <th994209@gmail.com>
2026-01-18 23:17:02 +0900

    パスワード変更と退会用の専用画面を作成

---
+++ app/controllers/account/passwords_controller.rb
@@ -0,0 +1,21 @@
+class Account::PasswordsController < ApplicationController
+  before_action :authenticate_user!
+
+  def edit
+  end
+
+  def update
+    if current_user.update_with_password(password_params)
+      bypass_sign_in current_user
+      redirect_to account_password_path, notice: "Password updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
+  end
+end
