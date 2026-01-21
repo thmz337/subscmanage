d74b20943644e50a5b512f1fe8820c65508d8ee0
thmz337 <th994209@gmail.com>
2026-01-21 17:09:21 +0900

    ユーザー登録画面で、フォームにメールアドレス確認用の欄を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,22 @@ class User < ApplicationRecord
 
   enum :role, [ :general, :admin ]
 
+  attr_accessor :email_confirmation
+
+  validates :email_confirmation, presence: true, on: :create
+  validate :email_matches_confirmation, on: :create
+
   def admin?
     role == "admin"
   end
 
   def this_month_payment_services
     subscription_services.filter { |service| service.next_payment.month == Date.today.month }
+  end
+
+  private
+
+  def email_matches_confirmation
+    errors.add(:email_confirmation, :confirmation, message: "が一致しません") if email != email_confirmation
   end
 end
