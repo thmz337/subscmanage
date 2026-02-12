fc189a3977a1a7985a22fd5d10e209967be1bc96
thmz337 <th994209@gmail.com>
2026-02-13 00:08:18 +0900

    ログインページのデザインを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def flash_css_class(key)
-    base = "mb-4 rounded px-4 py-3"
+    base = "mx-auto max-w-md px-2 py-2 rounded sm:px-6 lg:px-8"
     case key.to_sym
     when :notice
       "#{base} bg-green-100 text-green-800"
