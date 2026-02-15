fb083dbd9fa000f06f4cafd0ba66682ee88a6e53
thmz337 <th994209@gmail.com>
2026-02-15 20:49:09 +0900

    パスワード変更画面のデザインを修正

--- app/controllers/account/passwords_controller.rb
+++ app/controllers/account/passwords_controller.rb
@@ -7,7 +7,7 @@ class Account::PasswordsController < ApplicationController
   def update
     if current_user.update_with_password(password_params)
       bypass_sign_in current_user
-      redirect_to account_password_path, notice: "Password updated successfully"
+      redirect_to root_path, notice: t("devise.passwords.edit.message.password_changed")
     else
       render :edit, status: :unprocessable_entity
     end
