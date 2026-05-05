1e1edf16bd31583278b4c785843fe3adc22bbd42
thmz337 <th994209@gmail.com>
2026-05-05 10:18:20 +0900

    lintの指摘事項を修正

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -1,16 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    this.timer = setTimeout(() => this.dismiss(), 4000)
+    this.timer = setTimeout(() => this.dismiss(), 4000);
   }
 
   disconnect() {
-    clearTimeout(this.timer)
+    clearTimeout(this.timer);
   }
 
   dismiss() {
-    this.element.classList.add("opacity-0", "translate-x-4")
-    setTimeout(() => this.element.remove(), 300)
+    this.element.classList.add("opacity-0", "translate-x-4");
+    setTimeout(() => this.element.remove(), 300);
   }
 }
